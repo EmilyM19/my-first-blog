2913587d9076a95d9d8fd7595e47c58f06798100
Emily Mullins <emullins19@ivytech.edu>
2023-12-10 16:08:37 -0500

    M07 Extemd your application

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-'''assigning a view called post_list to the root URL'''
-
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
